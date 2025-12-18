c1dcfe9585adc8e1de84c11e0c2450f102d9063c
kardelio <kardeliogithub@outlook.com>
2025-12-18 17:49:39 +0100

    updw

--- action/git_update.sh
+++ action/git_update.sh
@@ -4,7 +4,16 @@ VERSION=""
 
 
 echo "Yeh"
+while getopts v: flag
+do
+    case "${flag}" in
+        v) 
+            VERSION=${OPTARG}
+            ;;
+        esac
+done
 
+echo "---------------${VERSION}"
 NEWTAG="1.2.3"
 
 
